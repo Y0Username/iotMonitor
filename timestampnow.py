d7f738acbe4a25f8e62e7dc6b324dc687816ffae
Yathish <ygangoll@uci.edu>
2017-06-03 18:36:32 -0700

    Payload edit

--- timestampnow.py
+++ timestampnow.py
@@ -1,12 +1,12 @@
 import datetime
 
 def get_time():
-	current_date_time = datetime.datetime.now()
+	time_now = datetime.datetime.now()
 	date_time = {}
-	date_time["id"]= str(currentdatetime)
-	date_time["month"]= currentdatetime.month
-	date_time["weekday"]= currentdatetime.weekday()
-	date_time["hour"]= currentdatetime.hour
-	date_time["minute"]= currentdatetime.minute
-	date_time['day'] = currentdatetime.day
+	date_time["id"]= str(time_now)
+	date_time["month"]= time_now.month
+	date_time["weekday"]= time_now.weekday()
+	date_time["hour"]= time_now.hour
+	date_time["minute"]= time_now.minute
+	date_time['day'] = time_now.day
 	return date_time;
